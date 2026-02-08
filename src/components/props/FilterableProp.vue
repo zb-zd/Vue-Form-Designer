e1e6588ae4c4b64c4a02c0e130ab6234ddba8cf1
日渐肥胖的左 <2712104231@qq.com>
2026-02-08 14:12:26 +0800

    feat: 添加 FilterableProp 和 MultipleProp 组件，支持布尔值切换

---
+++ src/components/props/FilterableProp.vue
@@ -0,0 +1,18 @@
+<template>
+  <el-switch
+    :model-value="modelValue"
+    @update:model-value="$emit('update:modelValue', $event)"
+  />
+</template>
+
+<script setup lang="ts">
+interface Props {
+  modelValue: boolean
+}
+
+defineProps<Props>()
+
+defineEmits<{
+  'update:modelValue': [value: boolean]
+}>()
+</script>
